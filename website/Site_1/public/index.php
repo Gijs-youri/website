9fc2a430bc098f03f6778f09307cc858786a2bdd
Gijser <57718086+Gijser@users.noreply.github.com>
2019-11-18 11:11:29 +0100

    Add files via upload

---
+++ website/Site_1/public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>index</title>
+        <link rel="stylesheet" media="all" href="stylesheets/main.css" />
+    </head>
+
+<body>
+
+<header>
+    <h1>GC Festival Tickets</h1>
+</header>
+
+<navigation>
+    <ul>
+        <li><a href="index.php">Menu</a></li>
+    </ul>
+</navigation>
+
+<div id="content"></div>
+
+<footer>
+    &copy; <?php echo date("Y"); ?> Graafschap College
+</footer>
+
+</body>
+
+</html>
