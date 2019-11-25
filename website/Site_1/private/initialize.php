91dba6ced34d94e07bd3add642ac7aea07501d54
D3ADFAC3D <57796104+D3ADFAC3D@users.noreply.github.com>
2019-11-25 11:18:30 +0100

    1

--- website/Site_1/private/initialize.php
+++ website/Site_1/private/initialize.php
@@ -1,12 +1,20 @@
-<?php require_once("functions.php");?>
-<?php $page_title = "GC Festival Tickets";?>
-<?php require("../private/shared/header.php");?>
-<div id="content"></div>
-<?php require("../private/shared/footer.php");?>
-
 <?php
 define("PRIVATE_PATH", dirname(__File__));
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
-define("PUBLIC_PATH", PROJECT_PATH .('/public'));
-define("SHARED_PATH", PRIVATE_PATH .('/shared'));
+define("PUBLIC_PATH", PROJECT_PATH . ('/public'));
+define("SHARED_PATH", PRIVATE_PATH . ('/shared'));
 ?>
+
+
+<?php require_once("functions.php");?>
+<?php $page_title = "GC Festival Tickets";?>
+<?php require(SHARED_PATH . "/header.php");?>
+<div id="content">
+    <div id = "main-menu">
+        <h2>Main Menu</h2>
+        <ul>
+            <li><a href="../public/index.php">Hoofdpagina</a></li>
+        </ul>
+    </div>
+</div>
+<?php require(SHARED_PATH . "/footer.php");?>
