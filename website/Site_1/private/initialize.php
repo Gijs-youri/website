5ec12f896f277326f8f6df33345f70c86b422490
D3ADFAC3D <57796104+D3ADFAC3D@users.noreply.github.com>
2019-11-25 11:41:13 +0100

    1

--- website/Site_1/private/initialize.php
+++ website/Site_1/private/initialize.php
@@ -8,13 +8,3 @@ define("SHARED_PATH", PRIVATE_PATH . ('/shared'));
 
 <?php require_once("functions.php");?>
 <?php $page_title = "GC Festival Tickets";?>
-<?php require(SHARED_PATH . "/header.php");?>
-<div id="content">
-    <div id = "main-menu">
-        <h2>Main Menu</h2>
-        <ul>
-            <li><a href="../public/index.php">Hoofdpagina</a></li>
-        </ul>
-    </div>
-</div>
-<?php require(SHARED_PATH . "/footer.php");?>
--- website/Site_1/public/index.php
+++ website/Site_1/public/index.php
@@ -1,1 +1,13 @@
 <?php require("../private/initialize.php") ?>
+<?php require(SHARED_PATH . "/header.php");?>
+
+<div id="content">
+    <div id = "main-menu">
+        <h2>Main Menu</h2>
+        <ul>
+            <li><a href="public/index.php">Hoofdpagina</a></li>
+        </ul>
+    </div>
+</div>
+
+<?php require(SHARED_PATH . "/footer.php");?>
