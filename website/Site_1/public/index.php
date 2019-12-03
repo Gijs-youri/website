08ed15240f3ed390a2b3d540be602dcb90e4778e
D3ADFAC3D <57796104+D3ADFAC3D@users.noreply.github.com>
2019-12-03 08:16:56 +0100

    1

--- website/Site_1/public/index.php
+++ website/Site_1/public/index.php
@@ -10,12 +10,10 @@
             <li><a href="<?php echo url_for('/tickets.php'); ?>">Tickets</a></li>
             <li><a href="<?php echo url_for('/profiel.php'); ?>">Profiel</a></li>
 
-
-<!--            <li><a href="--><?php //echo url_for('/contact.php'); ?><!--">Contact</a></li>-->
-<!--            <li><a href="--><?php //echo url_for('/about.php'); ?><!--">About</a></li>-->
-
         </ul>
     </div>
 </div>
 
 <?php require(SHARED_PATH . "/footer.php");?>
+
+<!--W3-schools -> Html forms-->
--- website/Site_1/public/login.php
+++ website/Site_1/public/login.php
@@ -4,6 +4,13 @@
 <div id="content">
     <h2>Inloggen</h2>
     <p>Hier kunt u later inloggen.</p>
+
+    <form>
+        First name: <br>
+        <input type="text" name="firstname"><br>
+        Last name: <br>
+        <input type="text" name="lastname">
+    </form>
 </div>
 
 <?php require(SHARED_PATH . "/footer.php");?>
